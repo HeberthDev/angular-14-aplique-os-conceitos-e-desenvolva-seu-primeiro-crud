f35f6abc16a82acd0a24b7a3e730c76abfee0519
HeberthDev <heberthdeveloper@gmail.com>
2025-01-28 07:23:12 -0300

    Requisições com HttpClient

--- memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -9,9 +10,11 @@ import { Pensamento } from '../pensamento';
 export class ListarPensamentoComponent implements OnInit {
   public listaPensamentos: Pensamento[] = [];
 
-  constructor() { }
+  constructor(
+    private pensamentoService: PensamentoService,
+  ) { }
 
   ngOnInit(): void {
+    this.pensamentoService.listar();
   }
-
 }
